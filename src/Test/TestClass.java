af99ed5f38ab2ee9dca3c7b0dcd76eed800bc882
aminthabti95 <37442246+aminthabti95@users.noreply.github.com>
2020-09-07 16:55:44 +0200

    Update TestClass.java

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -3,10 +3,22 @@ package Test;
 import org.openqa.selenium.WebDriver;
 
 
-@Test
 public class TestClass  {
-	
-	public  static WebDriver driver ;
+	public static void main(String[] args) {
+	       System.setProperty("webdriver.chrome.driver","/home/amin/Downloads/chromedriver");	// <-- Change this path
+	       WebDriver driver = new ChromeDriver();
+	       String baseUrl = "https://experitest.com/free-trial/";
+	       String expectedTitle = "Free trial";
+	       String actualTitle = "";
+	       driver.get(baseUrl);
+	       actualTitle = driver.getTitle();
+	       if (actualTitle.contentEquals(expectedTitle)){
+	           System.out.println("TEST PASSED!");
+	       } else {
+	           System.out.println("TEST FAILED");
+	       }
+	       driver.close();
+	   }	
 	
 
 }
